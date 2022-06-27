ed2a880498a8e8abce48dca4db457579815ee6f9
yurakulinichenko <104848480+yurakulinichenko@users.noreply.github.com>
2022-06-27 19:45:52 +0300

    second commit

--- js/script.js
+++ js/script.js
@@ -3,13 +3,20 @@ let person = {};
 (async () => {
   let response = await fetch("https://yurii-cv-api.azurewebsites.net/");
   person = await response.json();
-  console.log(person);
-  let position = (document.querySelector(".about-position").innerHTML =
+  let position = (document.querySelector(".about-position").innerText =
     person.position);
-  let name = (document.querySelector(".about-name").innerHTML =
+  let name = (document.querySelector(".about-name").innerText =
     person.firstName + " " + person.lastName);
-  let tel = (document.querySelector(".tel").innerHTML =
+  let tel = (document.querySelector(".tel").innerText =
     person.contacts[1].value);
-  let mail = (document.querySelector(".mail").innerHTML =
+  let mail = (document.querySelector(".mail").innerText =
     person.contacts[0].value);
+})();
+(async () => {
+  let response = await fetch(
+    "https://yurii-cv-api.azurewebsites.net/experience"
+  );
+  let workExp = await response.json();
+  console.log(workExp);
+  workExp.forEach();
 })();
