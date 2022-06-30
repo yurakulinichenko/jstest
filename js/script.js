94ed15123419019440f550b7d6e38c2eae45ca96
yurakulinichenko <104848480+yurakulinichenko@users.noreply.github.com>
2022-06-30 16:11:12 +0300

    first resubmit

--- js/script.js
+++ js/script.js
@@ -1,113 +1,113 @@
 "use strict";
-let person = {};
+
 (async () => {
-  let response = await fetch("https://yurii-cv-api.azurewebsites.net/");
-  person = await response.json();
-  let position = (document.querySelector(".about-position").innerText =
+  const response = await fetch("https://yurii-cv-api.azurewebsites.net/");
+  const person = await response.json();
+  const position = (document.querySelector(".about-position").innerText =
     person.position);
-  let name = (document.querySelector(".about-name").innerText =
+  const name = (document.querySelector(".about-name").innerText =
     person.firstName + " " + person.lastName);
-  let tel = (document.querySelector(".tel").innerText =
+  const tel = (document.querySelector(".tel").innerText =
     person.contacts[1].value);
-  let mail = (document.querySelector(".mail").innerText =
+  const mail = (document.querySelector(".mail").innerText =
     person.contacts[0].value);
 })();
 (async () => {
-  let response = await fetch(
+  const response = await fetch(
     "https://yurii-cv-api.azurewebsites.net/experience"
   );
-  let workExp = await response.json();
+  const workExp = await response.json();
 
-  let exp = document.querySelector(".work");
+  const experience = document.querySelector(".work");
 
-  let work = workExp.reverse().map((value, index) => {
-    let title = document.createElement("h4");
+  workExp.reverse().forEach((value, index) => {
+    const title = document.createElement("h4");
     title.classList.add("position");
     title.innerHTML = workExp[index].title;
-    let company = document.createElement("span");
+    const company = document.createElement("span");
     company.innerHTML = " " + workExp[index].company;
     company.classList.add("organisation");
     title.appendChild(company);
-    exp.appendChild(title);
+    experience.appendChild(title);
 
-    let period = document.createElement("p");
+    const period = document.createElement("p");
     period.innerHTML =
       moment(workExp[index].from).format("MMMM YYYY") +
       " - " +
       moment(workExp[index].to).format("MMMM YYYY");
     period.classList.add("period");
-    let line = document.createElement("span");
+    const line = document.createElement("span");
     line.classList.add("line");
     line.innerText = " | ";
     period.appendChild(line);
-    exp.appendChild(period);
-    let country = workExp[index].country;
+    experience.appendChild(period);
+    const country = workExp[index].country;
     period.append(country);
 
-    let responsib = document.createElement("ul");
-    responsib.classList.add("work-list");
+    const responsibilities = document.createElement("ul");
+    responsibilities.classList.add("work-list");
     let arr = workExp[index].responsibilities;
-    for (var i = 0; i < arr.length; i++) {
-      var li = document.createElement("li");
+    for (let i = 0; i < arr.length; i++) {
+      const li = document.createElement("li");
       li.classList.add("work-list-item");
-      responsib.appendChild(li);
+      responsibilities.appendChild(li);
       li.innerHTML = li.innerHTML + arr[i];
     }
-    exp.appendChild(responsib);
+    experience.appendChild(responsibilities);
   });
 })();
 
 (async () => {
-  let response = await fetch(
+  const response = await fetch(
     "https://yurii-cv-api.azurewebsites.net/education"
   );
-  let education = await response.json();
+  const education = await response.json();
 
-  let educ = document.querySelector(".education");
-  let university = education.highEducations.map((value, index) => {
-    let univer = document.createElement("p");
+  const highEducation = document.querySelector(".education");
+  education.highEducations.forEach((value, index) => {
+    const univer = document.createElement("p");
     univer.classList.add("university");
     univer.innerHTML = education.highEducations[index].university;
-    educ.appendChild(univer);
+    highEducation.appendChild(univer);
 
-    let faculty = document.createElement("p");
+    const faculty = document.createElement("p");
     faculty.classList.add("faculty");
     faculty.innerHTML = education.highEducations[index].specialization;
-    educ.appendChild(faculty);
+    highEducation.appendChild(faculty);
 
-    let period = document.createElement("p");
+    const period = document.createElement("p");
     period.innerHTML =
       education.highEducations[index].from +
       " - " +
       education.highEducations[index].to;
     period.classList.add("years");
-    let line = document.createElement("span");
+    const line = document.createElement("span");
     line.classList.add("line");
     line.innerText = " | ";
     period.appendChild(line);
-    educ.appendChild(period);
-    let country = education.highEducations[index].country;
+    highEducation.appendChild(period);
+    const country = education.highEducations[index].country;
     period.append(country);
   });
 })();
 
 (async () => {
-  let response = await fetch(
+  const response = await fetch(
     "https://yurii-cv-api.azurewebsites.net/education"
   );
-  let training = await response.json();
-  let course = document.querySelector(".training");
-  let trainingCourses = training.trainings.map((value, index) => {
-    let org = document.createElement("p");
-    org.classList.add("course-org");
-    org.innerHTML = training.trainings[index].organization;
-    course.appendChild(org);
+  const training = await response.json();
+  const course = document.querySelector(".training");
+  const trainingCourses = training.trainings.map((value, index) => {
+    const organisation = document.createElement("p");
+    organisation.classList.add("course-org");
+    organisation.innerHTML = training.trainings[index].organization;
+    course.appendChild(organisation);
 
-    let courseList = document.createElement("ul");
+    const courseList = document.createElement("ul");
     courseList.classList.add("course-list");
-    let arr = training.trainings[index].subjects;
+    const arr = training.trainings[index].subjects;
     for (let i = 0; i < arr.length; i++) {
-      let li = document.createElement("li");
+      const li = document.createElement("li");
       li.classList.add("course-list-item");
       courseList.appendChild(li);
       li.innerHTML = li.innerHTML + arr[i];
@@ -117,38 +117,36 @@ let person = {};
 })();
 
 (async () => {
-  let response = await fetch("https://yurii-cv-api.azurewebsites.net/skills");
-  let skills = await response.json();
-  let hard = skills.filter((index, type) => index.type == "Hard");
-  let soft = skills.filter((index, type) => index.type == "Soft");
+  const response = await fetch("https://yurii-cv-api.azurewebsites.net/skills");
+  const skills = await response.json();
+  const hard = skills.filter((skills) => skills.type == "Hard");
+  const soft = skills.filter((skills) => skills.type == "Soft");
 
-  let techSkills = document.querySelector(".tech-skills");
-  let hardSkills = document.createElement("ul");
+  const techSkills = document.querySelector(".tech-skills");
+  const hardSkills = document.createElement("ul");
   hardSkills.classList.add("tech-skills-list");
-  let arr = hard;
-  for (let i = 0; i < arr.length; i++) {
-    let li = document.createElement("li");
+  for (let i = 0; i < hard.length; i++) {
+    const li = document.createElement("li");
     li.classList.add("tech-skills-item");
     hardSkills.appendChild(li);
-    let hardList = document.createElement("span");
+    const hardList = document.createElement("span");
     hardList.classList.add("tech-skills-text");
-    hardList.innerHTML = arr[i].name;
+    hardList.innerHTML = hard[i].name;
     li.appendChild(hardList);
   }
   techSkills.appendChild(hardSkills);
 
-  let softSkills = document.querySelector(".soft-skills");
-  let softSk = document.createElement("ul");
-  softSk.classList.add("soft-skills-list");
-  let array = soft;
-  for (let i = 0; i < array.length; i++) {
-    let li = document.createElement("li");
+  const softSkills = document.querySelector(".soft-skills");
+  const softSkillsList = document.createElement("ul");
+  softSkillsList.classList.add("soft-skills-list");
+  for (let i = 0; i < soft.length; i++) {
+    const li = document.createElement("li");
     li.classList.add("soft-skills-item");
-    softSk.appendChild(li);
-    let softList = document.createElement("span");
+    softSkillsList.appendChild(li);
+    const softList = document.createElement("span");
     softList.classList.add("soft-skills-text");
-    softList.innerHTML = array[i].name;
+    softList.innerHTML = soft[i].name;
     li.appendChild(softList);
   }
-  softSkills.appendChild(softSk);
+  softSkills.appendChild(softSkillsList);
 })();
